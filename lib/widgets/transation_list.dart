a73ae6116e8ef8914f8f4453f7d3f698758cca31
Clifford Teneng <cliffordteng5@gmail.com>
2019-11-28 18:10:28 +0100

    improved in the UI chartbar and chart

--- lib/widgets/transation_list.dart
+++ lib/widgets/transation_list.dart
@@ -31,52 +31,77 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemBuilder: (ctx, index) {
                 return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 15,
-                        ),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                          color: Theme.of(context).primaryColor,
-                          width: 2,
-                        )),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          '\$${transations[index].amount.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Theme.of(context).primaryColor,
-                          ),
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text('\$${transations[index].amount}'),
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            transations[index].title,
-                            // style: TextStyle(
-                            //   fontWeight: FontWeight.bold,
-                            //   fontSize: 16,
-                            style: Theme.of(context).textTheme.title,
-                            // ),
-                          ),
-                          Text(
-                            DateFormat.yMMMd().format(transations[index].date),
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 10,
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
+                    ),
+                    title: Text(
+                      transations[index].title,
+                      style: Theme.of(context).textTheme.title,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMM().format(transations[index].date),
+                    ),
                   ),
                 );
+                // return Card(
+                //   child: Row(
+                //     children: <Widget>[
+                //       Container(
+                //         margin: EdgeInsets.symmetric(
+                //           vertical: 10,
+                //           horizontal: 15,
+                //         ),
+                //         decoration: BoxDecoration(
+                //             border: Border.all(
+                //           color: Theme.of(context).primaryColor,
+                //           width: 2,
+                //         )),
+                //         padding: EdgeInsets.all(10),
+                //         child: Text(
+                //           '\$${transations[index].amount.toStringAsFixed(2)}',
+                //           style: TextStyle(
+                //             fontWeight: FontWeight.bold,
+                //             fontSize: 20,
+                //             color: Theme.of(context).primaryColor,
+                //           ),
+                //         ),
+                //       ),
+                //       Column(
+                //         crossAxisAlignment: CrossAxisAlignment.start,
+                //         children: <Widget>[
+                //           Text(
+                //             transations[index].title,
+                //             // style: TextStyle(
+                //             //   fontWeight: FontWeight.bold,
+                //             //   fontSize: 16,
+                //             style: Theme.of(context).textTheme.title,
+                //             // ),
+                //           ),
+                //           Text(
+                //             DateFormat.yMMMd().format(transations[index].date),
+                //             style: TextStyle(
+                //               fontWeight: FontWeight.bold,
+                //               fontSize: 10,
+                //               color: Colors.grey,
+                //             ),
+                //           ),
+                //         ],
+                //       )
+                //     ],
+                //   ),
+                // );
               },
               itemCount: transations.length,
             ),
