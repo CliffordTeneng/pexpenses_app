ff4ff5e7b62d1a1d8207bdabc9893ea22fe3c7ed
cliffordTeneng <tenengclifford5@gmail.com>
2020-01-06 20:33:50 +0100

    modified chart_bar.dart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import '../widgets/chart_bar.dart';
 import '../models/transaction.dart';
+import './chart_bar.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
 
   Chart(this.recentTransactions);
 
-  List<Map<String, Object>> get groupedTransactionValues {
+  List<Map<String, Object>> get groupTransactionValues {
     return List.generate(7, (index) {
       final weekDay = DateTime.now().subtract(
         Duration(days: index),
       );
-      double totalSum;
+      double totalSum = 0.0;
 
       for (var i = 0; i < recentTransactions.length; i++) {
         if (recentTransactions[i].date.day == weekDay.day &&
@@ -24,14 +24,15 @@ class Chart extends StatelessWidget {
       }
 
       return {
-        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        "day": DateFormat.E().format(weekDay).substring(0, 1),
         'amount': totalSum,
       };
     }).reversed.toList();
   }
 
   double get totalSpending {
-    return groupedTransactionValues.fold(0.0, (sum, item) {
+    return groupTransactionValues.fold(0.0, (sum, item) {
+      print(sum);
       return sum + item['amount'];
     });
   }
@@ -41,19 +42,20 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Container(
+      child: Padding(
         padding: EdgeInsets.all(10),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: groupedTransactionValues.map((data) {
+          children: groupTransactionValues.map((data) {
             return Flexible(
               fit: FlexFit.tight,
               child: ChartBar(
-                  data['day'],
-                  data['amount'],
-                  totalSpending == 0.0
-                      ? 0.0
-                      : (data['amount'] as double) / totalSpending),
+                data['day'],
+                data['amount'],
+                totalSpending == 0.0
+                    ? 0.0
+                    : (data['amount'] as double) / totalSpending,
+              ),
             );
           }).toList(),
         ),
